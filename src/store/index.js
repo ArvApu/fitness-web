3c513c1d13b3a71cad3d78810bcb74be7d63cabd
arvapu <arvydas.apulskis123@gmail.com>
2021-02-25 17:55:11 +0200

    persistedstate installed

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
+const persistedState = createPersistedState({
+  key: 'fitness_app_state',
+});
+
 export default new Vuex.Store({
   state: {
+    user: { penis: 'large' }
   },
   mutations: {
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins: [persistedState]
 })
