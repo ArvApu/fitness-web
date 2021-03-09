3cd6b9907cf620b1b6254c60e7b29e673b8d1ac6
arvapu <arvydas.apulskis123@gmail.com>
2021-03-09 11:44:04 +0200

    initial design for exercises completed

--- src/views/Home/Exercises.vue
+++ src/views/Home/Exercises.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
+  <div class="content-box">
 
-    <button ></button>
+    <div>
 
-    <div v-for="exercise in exercises" :key="exercise.id">
+      <button class="btn btn-primary"> Add exercise </button>
 
-<!--      class="form-input btn btn-primary"-->
+      <div class="exercises">
 
-      {{ exercise.id }}
-      {{ exercise.name }}
-      {{ exercise.description }}
-      {{ exercise.is_private }}
+        <div class="exercise" v-for="exercise in exercises" :key="exercise.id">
+
+          <div class="info">
+
+            <h3> {{ exercise.name }} </h3>
+
+            <div class="description">
+              <!--            {{ exercise.description }}-->
+              error sit quia labore sit ut et consectetur vero excepturi  error sit quia labore sit ut et consectetur vero excepturi error sit quia labore sit ut et consectetur vero excepturi error sit quia labore sit ut et consectetur vero excepturi error sit quia lab
+            </div>
+          </div>
+
+          <div class="control">
+            <font-awesome-icon icon="pen" size="2x"/>
+            <font-awesome-icon icon="trash-alt" size="2x"/>
+
+<!--            <div>-->
+<!--              <font-awesome-icon icon="pen" />-->
+<!--            </div>-->
+
+<!--            <div>-->
+<!--              <font-awesome-icon icon="trash-alt" />-->
+<!--            </div>-->
+            <!--          {{ exercise.id }}-->
+          </div>
+
+        </div>
+      </div>
+
     </div>
 
   </div>
@@ -44,3 +69,45 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  .exercises {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .exercise {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    box-sizing: border-box;
+    margin: 1rem 0.6em;
+    flex: 0 1 48%;
+    transition: 0.3s;
+    height: 130px;
+    padding: 7px;
+    display: flex;
+  }
+
+  .exercise h3 {
+    padding: 0;
+    margin: 0;
+  }
+
+  .exercise .info {
+    width: 90%;
+    padding: 5px;
+    border-right: 2px solid #999999;
+  }
+
+  .exercise .info .description {
+    margin: 10px 0;
+  }
+
+  .exercise .control {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    width: 10%;
+  }
+</style>
