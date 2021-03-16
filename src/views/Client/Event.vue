22e1945fb1e049ed64544d274cc38b3b20c8442e
arvapu <arvydas.apulskis123@gmail.com>
2021-03-16 20:26:29 +0200

    implemented event delete

--- src/views/Client/Event.vue
+++ src/views/Client/Event.vue
@@ -2,6 +2,15 @@
 
   <div>
     {{event}}
+
+    <div v-if="canControl">
+      <button v-on:click="edit" class="btn btn-secondary"> Edit </button>
+      <button v-on:click="remove" class="btn btn-danger"> Remove </button>
+    </div>
+
+    <!-- MODALS -->
+    <v-dialog/>
+
   </div>
 
 </template>
@@ -15,17 +24,44 @@ export default {
     return {
       id: this.$route.params.id,
       event: null,
+      canControl: ['trainer', 'admin'].includes(this.$store.state.auth.user.role),
     }
   },
   methods: {
     ...mapActions('events', [
-      'fetchOne'
-    ])
+      'fetchOne', 'delete'
+    ]),
+    edit() {
+      console.log('edit');
+    },
+    remove() {
+      this.$modal.show('dialog', {
+        title: 'WARNING',
+        text: 'Are you sure you want to delete this event?',
+        buttons: [
+          {
+            title: 'Cancel',
+            handler: () => {
+              this.$modal.hide('dialog');
+            }
+          },
+          {
+            title: 'Yes',
+            handler: () => {
+              this.$modal.hide('dialog');
+              this.delete(this.id).then(() => this.$router.push({ name: 'Calendar'}));
+            }
+          },
+        ]
+      })
+    },
   },
   created() {
     this.fetchOne(this.id).then((result) => {
       this.event = result;
-    })
+    }).catch(() => {
+      this.$router.back();
+    });
   }
 }
 </script>
